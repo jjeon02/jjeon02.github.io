b2ead86c6cb84fee3dfe092a4ea28c614878329c
Jueun Jeon <jjeon02@risd.edu>
2019-11-30 21:31:31 -0800

    Update index.js

--- Cinema/index.js
+++ Cinema/index.js
@@ -1,44 +1,4 @@
-var counter=0;
-var timeleft= 60;
-
-function convertSeconds(s){
-    var min = floor(s/60);
-    var sec = s % 60 ;
-    return min + ' ' + 'min' + ' ' + sec + ' ' + 'sec';
-    
+function myFunction() {
+    // var x = document.getElementById("myText").value;
+    document.getElementById("answer").innerHTML = 'Thank you for your submission ! ';
 }
-
-function setup(){
-    var timer = select('#timer');
-    timer.html(convertSeconds(counter));
-    // html id & javascript
-
-    function timeIt(){
-        counter++;
-        timer.html(convertSeconds(counter));
-    }
-    //every second increase the counter & update dom element
-
-    setInterval(timeIt,1000);
-    // want this even to happen every milli-seconds
-}
-
-
-// countup
-
-// let counter = 0;
-
-// function setup(){
-// //   noCanvas();//limit animation
-  
-//   var timer = select('#timer');
-//   timer.html('0'); //insert text
-  
-//   function timeIt(){
-//     counter ++;
-//     timer.html(counter);
-//   }
-  
-//   setInterval(timeIt, 1000);//native function 1000ms =1 s
-//   //request animationframe is also another func
-// }
