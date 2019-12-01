c97cd2b0dd9af05dbca03f438bfc9db728059c69
Jueun Jeon <jjeon02@risd.edu>
2019-11-30 16:50:25 -0800

    Update index.js

--- Cinema/index.js
+++ Cinema/index.js
@@ -1,31 +1,44 @@
-// window.onload = function() {
-//     // Month Day, Year Hour:Minute:Second, id-of-element-container
-//     countUpFromTime("00:00:00", 'countup1'); // ****** Change this line!
-//   };
-//   function countUpFromTime(countFrom, id) {
-//     countFrom = new Date(countFrom).getTime();
-//     var now = new Date(),
-//         countFrom = new Date(countFrom),
-//         timeDifference = (now - countFrom);
-      
-//     var secondsInADay = 60 * 60 * 1000 * 24,
-//         secondsInAHour = 60 * 60 * 1000;
-      
-//     // days = Math.floor(timeDifference / (secondsInADay) * 1);
-//     hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
-//     mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
-//     secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
-  
-//     var idEl = document.getElementById(id);
-//     // idEl.getElementsByClassName('days')[0].innerHTML = days;
-//     idEl.getElementsByClassName('hours')[0].innerHTML = hours;
-//     idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
-//     idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
-  
-//     clearTimeout(countUpFromTime.interval);
-//     countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
-//   }
+var counter=0;
+var timeleft= 60;
+
+function convertSeconds(s){
+    var min = floor(s/60);
+    var sec = s % 60 ;
+    return min + ' ' + 'min' + ' ' + sec + ' ' + 'sec';
+    
+}
 
 function setup(){
+    var timer = select('#timer');
+    timer.html(convertSeconds(counter));
+    // html id & javascript
 
+    function timeIt(){
+        counter++;
+        timer.html(convertSeconds(counter));
+    }
+    //every second increase the counter & update dom element
+
+    setInterval(timeIt,1000);
+    // want this even to happen every milli-seconds
 }
+
+
+// countup
+
+// let counter = 0;
+
+// function setup(){
+// //   noCanvas();//limit animation
+  
+//   var timer = select('#timer');
+//   timer.html('0'); //insert text
+  
+//   function timeIt(){
+//     counter ++;
+//     timer.html(counter);
+//   }
+  
+//   setInterval(timeIt, 1000);//native function 1000ms =1 s
+//   //request animationframe is also another func
+// }
