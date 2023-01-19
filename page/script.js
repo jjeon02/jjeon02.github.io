dcf1fc8fc962571d20f329910b76558a00f5c366
jjeon02 <june.jeon@gmail.com>
2023-01-19 10:51:59 -0500

    organize js

--- page/script.js
+++ page/script.js
@@ -1,3 +1,35 @@
+let contact = "<strong class='center'>CONTACT ME:</strong><br>"
+let linkedin = "<a href='https://www.linkedin.com/in/jueun-jeon-0436/' target='_blank'>" + "LinkedIn"+"</a>";
+let behance = "<a href='https://www.behance.net/jueunjeon' target='_blank'>" + "Behance" + "</a>";
+let email = "<a href='mailto:jjeon7@sva.edu' target='_blank'>" + "Email" + "</a>";
+let resume = "<a href='about.html#resume'>" + "resume" + "</a>";
+let github = "<a href='https://github.com/jjeon02' target='_blank'>" + "Github" + "</a>";
+
+let footerText = contact + linkedin + " / " + behance + " / " + email + " / " + resume + " / " + github;
+
+document.getElementById("footer").innerHTML = footerText;
+console.log(footerText);
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 10;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+    console.log("reveal working")
+  }
+  
+window.addEventListener("scroll", reveal);
+
 // // NAVIGATION
 function openNav() {
   document.getElementById("sideNav").style.width= "100%";
@@ -7,26 +39,27 @@ function closeNav() {
   document.getElementById("sideNav").style.width = "0";
 }
 
+
 // SLIDE
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slides");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none"; 
+//   }
+//   slides[slideIndex-1].display = "block";  
+// }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
