b2420f110c69d338df00bdd44abf9a78b597341c
jjeon02 <june.jeon@gmail.com>
2023-02-02 10:49:20 -0500

    update canvas.js

--- canvas.js
+++ canvas.js
@@ -27,3 +27,42 @@ function draw() {
 
 function clearArt(){clear();}
 function saveArt(){save('jueun-jeon-art.jpg');}
+
+let contact = "<strong class='center'>CONTACT ME:</strong><br>"
+let linkedin = "<a href='https://www.linkedin.com/in/jueun-jeon-0436/' target='_blank'>" + "LinkedIn"+"</a>";
+let behance = "<a href='https://www.behance.net/jueunjeon' target='_blank'>" + "Behance" + "</a>";
+let email = "<a href='mailto:jjeon7@sva.edu' target='_blank'>" + "Email" + "</a>";
+let resume = "<a href='about.html#resume'>" + "resume" + "</a>";
+let github = "<a href='https://github.com/jjeon02' target='_blank'>" + "Github" + "</a>";
+
+let footerText = contact + linkedin + " / " + behance + " / " + email + " / " + resume + " / " + github;
+
+document.getElementById("footer").innerHTML = footerText;
+console.log(footerText);
+
+function openNav() {
+    document.getElementById("sideNav").style.width= "100%";
+}
+  
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 10;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+    console.log("reveal working")
+  }
+  
+window.addEventListener("scroll", reveal);
