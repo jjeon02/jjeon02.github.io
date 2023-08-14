706549eb55573bad60e6802445841242333cfb54
jjeon02 <june.jeon@gmail.com>
2023-08-14 15:37:43 -0400

    update

---
+++ page/slide.js
@@ -0,0 +1,43 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slides");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "block"; 
+//   }
+//   slides[slideIndex-1].display = "none";  
+// }
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
