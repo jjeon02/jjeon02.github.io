921c0046ee70a8204404f8abff00565736cfbb71
jjeon02 <june.jeon@gmail.com>
2023-10-04 18:46:05 -0400

    update

--- canvas.js
+++ canvas.js
@@ -1,33 +1,3 @@
-var sketchWidth;
-var sketchHeight;
-let bgimg;
-
-
-function setup() {
-    sketchWidth = document.getElementById("canvas").offsetWidth;
-    sketchHeight =   document.getElementById("canvas").offsetHeight;
-
-    let renderer = createCanvas(sketchWidth, sketchHeight);
-    renderer.parent("canvas");
-
-    // image(img, 0, 0, sketchWidth, sketchHeight);
-    background(0, 0, 255);
-}
-
-function windowResized() {
-    sketchWidth = document.getElementById("square").offsetWidth;
-    sketchHeight = document.getElementById("square").offsetHeight;
-    resizeCanvas(sketchWidth, sketchHeight);
-}
-
-function draw() {
-    // background(bg);
-    ellipse(mouseX, mouseY, 20, 20);
-}
-
-function clearArt(){clear();}
-function saveArt(){save('jueun-jeon-art.jpg');}
-
 let contact = "<strong class='center'>CONTACT ME:</strong><br>"
 let linkedin = "<a href='https://www.linkedin.com/in/jueun-jeon-0436/' target='_blank'>" + "LinkedIn"+"</a>";
 let behance = "<a href='https://www.behance.net/jueunjeon' target='_blank'>" + "Behance" + "</a>";
